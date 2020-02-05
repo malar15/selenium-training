ec18c6912c006cbc9fe6d8e5bb81a413d82ceb39
Malarvizhi <fleur.lalithaa@gmail.com>
2020-02-04 20:08:37 -0800

    basics java

---
+++ Practise/src/basics.java
@@ -0,0 +1,16 @@
+
+public class basics {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Java Learning");
+int a=1;
+int b =3;
+int sum = a+b;
+System.out.println("Sum is "+sum);
+classobject obj=new classobject();
+obj.header();
+
+	}
+
+}
